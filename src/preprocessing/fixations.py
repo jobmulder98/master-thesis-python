246ca582f81b0361bec4a497909973d429d1061c
jobmulder98 <101323001+jobmulder98@users.noreply.github.com>
2023-08-30 17:35:39 +0200

    updated fixations

--- src/preprocessing/fixations.py
+++ src/preprocessing/fixations.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 from clean_raw_dataset import create_clean_dataframe
+from scipy.signal import butter, filtfilt, lfilter
 
 
 def add_delta_seconds_to_dataframe(dataframe):
@@ -16,19 +17,20 @@ def add_delta_seconds_to_dataframe(dataframe):
 
 def add_angle_to_dataframe(dataframe):
     dataframe["angleVelocity"] = 0
-    for i in range(len(dataframe["eyesDirection"])-1):
+    for i in range(len(dataframe["rayDirection"])-1):
         dataframe["angleVelocity"].iloc[i+1] = angle_between(
-            dataframe["eyesDirection"].iloc[i],
-            dataframe["eyesDirection"].iloc[i+1]
+            dataframe["rayDirection"].iloc[i],
+            dataframe["rayDirection"].iloc[i+1]
         )
+    # print(dataframe["angleVelocity"])
     return dataframe
 
 
 def add_degrees_per_second_to_dataframe(dataframe):
     dataframe = add_delta_seconds_to_dataframe(dataframe)
     dataframe = add_angle_to_dataframe(dataframe)
-    dataframe.loc[:, "degreesPerSecond"] = dataframe["angleVelocity"] / dataframe["deltaSeconds"]
-    dataframe.loc[:, "degreesPerSecond"].iloc[0] = 0
+    dataframe["degreesPerSecond"] = dataframe["angleVelocity"] / dataframe["deltaSeconds"]
+    dataframe["degreesPerSecond"].iloc[0] = 0
     return dataframe
 
 
@@ -48,7 +50,26 @@ def calculate_delta_time_seconds(time1, time2):
     return delta_time_seconds
 
 
+def butter_lowpass_filter(data):
+    fs = 50
+    cutoff = 10
+    nyq = 0.5 * fs
+    order = 2
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    y = filtfilt(b, a, data)
+    return y
+
+
 clean_dataframe = create_clean_dataframe()
 dataframe_with_added_columns = add_degrees_per_second_to_dataframe(clean_dataframe)
-plt.plot(dataframe_with_added_columns["degreesPerSecond"])
+# filtered_degrees_per_second = butter_lowpass_filter(dataframe_with_added_columns["degreesPerSecond"])
+# print(filtered_degrees_per_second)
+#
+# # quarter_of_the_data = int(len(filtered_degrees_per_second) / 4)
+# print(dataframe_with_added_columns["degreesPerSecond"])
+# # print(filtered_degrees_per_second)
+plt.plot(dataframe_with_added_columns["degreesPerSecond"]) #[:quarter_of_the_data])
+# # plt.plot(filtered_degrees_per_second)
+plt.axhline(100, color="red")
 plt.show()
