61c9a75021cef599c20043ff1d9eedd415e3d0ca
jobmulder98 <101323001+jobmulder98@users.noreply.github.com>
2023-09-12 14:03:05 +0200

    features finished including start and end indexes

--- src/preprocessing/ECG_EDA/ECG/features.py
+++ src/preprocessing/ECG_EDA/ECG/features.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 from biosppy.signals import ecg
 import os
-from scipy.stats import zscore, trapz
+from scipy.stats import zscore
 from scipy.interpolate import interp1d
+from scipy.integrate import trapz
 from scipy import signal
 from dotenv import load_dotenv
 
@@ -28,7 +29,11 @@ def mean_hrv_amplitude(dataframe, start_index, end_index):
     return dataframe["[B] HRV Amp."].iloc[start_index:end_index].mean()
 
 
-def detect_r_peaks(dataframe, sample_frequency=ECG_SAMPLE_RATE, plot=False):
+def detect_r_peaks(dataframe: pd.DataFrame,
+                   start_index: int,
+                   end_index: int,
+                   sample_frequency=ECG_SAMPLE_RATE,
+                   plot=False):
     filtered_ecg_signal = dataframe["Sensor-B:EEG-Filtered"].values
     r_peaks_uncorrected = ecg.ecg(filtered_ecg_signal,
                                   sampling_rate=sample_frequency,
@@ -64,82 +69,56 @@ def compute_rr_intervals(r_peaks, plot=False):
     return rr_corrected
 
 
-def time_domain_features(rr):
-    features = {}
-    hr = 60 / (rr / ECG_SAMPLE_RATE)
-    features['Mean RR (ms)'] = np.mean(rr)
-    features['STD RR/SDNN (ms)'] = np.std(rr)
-    features['Mean HR (beats/min)'] = np.mean(hr)
-    features['STD HR (beats/min)'] = np.std(hr)
-    features['Min HR (beats/min)'] = np.min(hr)
-    features['Max HR (beats/min)'] = np.max(hr)
-    features['RMSSD (ms)'] = np.sqrt(np.mean(np.square(np.diff(rr))))
-    features['NNxx'] = np.sum(np.abs(np.diff(rr)) > 50) * 1
-    features['pNNxx (%)'] = 100 * np.sum((np.abs(np.diff(rr)) > 50) * 1) / len(rr)
+def time_domain_features(dataframe: pd.DataFrame,
+                         start_index: int,
+                         end_index: int,
+                         plot_r_peaks=False,
+                         plot_rr_intervals=False) -> dict:
+    r_peaks = detect_r_peaks(dataframe, start_index, end_index, plot=plot_r_peaks)
+    rr_intervals = compute_rr_intervals(r_peaks, plot=plot_rr_intervals)
+    hr = 60 / (rr_intervals / ECG_SAMPLE_RATE)
+    features = {
+        "Mean RR (ms)": np.mean(rr_intervals),
+        "STD RR/SDNN (ms)": np.std(rr_intervals),
+        "Mean HR (beats/min)": np.mean(hr),
+        "STD HR (beats/min)": np.std(hr),
+        "Min HR (beats/min)": np.min(hr),
+        "Max HR (beats/min)": np.max(hr),
+        "RMSSD (ms)": np.sqrt(np.mean(np.square(np.diff(rr_intervals)))),
+        "NNxx": np.sum(np.abs(np.diff(rr_intervals)) > 50) * 1,
+        "pNNxx (%)": 100 * np.sum((np.abs(np.diff(rr_intervals)) > 50) * 1) / len(rr_intervals)
+    }
     return features
 
 
-def interpolate_rr_intervals(rr):
-    x = np.cumsum(rr) / ECG_SAMPLE_RATE
-    f = interp1d(x, rr, kind='cubic')
-    fs = 4.0
-    steps = 1 / fs
-    xx = np.arange(1, np.max(x), steps)
-    rr_interpolated = f(xx)
-    return rr_interpolated
-
-
-def frequency_domain(rri):
-    fxx, pxx = signal.welch(x=rri, fs=4)
-    cond_vlf = (fxx >= 0) & (fxx < 0.04)
-    cond_lf = (fxx >= 0.04) & (fxx < 0.15)
-    cond_hf = (fxx >= 0.15) & (fxx < 0.4)
-    vlf = trapz(pxx[cond_vlf], fxx[cond_vlf])
-    lf = trapz(pxx[cond_lf], fxx[cond_lf])
-    hf = trapz(pxx[cond_hf], fxx[cond_hf])
-    total_power = vlf + lf + hf
-
-    peak_vlf = fxx[cond_vlf][np.argmax(pxx[cond_vlf])]
-    peak_lf = fxx[cond_lf][np.argmax(pxx[cond_lf])]
-    peak_hf = fxx[cond_hf][np.argmax(pxx[cond_hf])]
-
-    lf_nu = 100 * lf / (lf + hf)
-    hf_nu = 100 * hf / (lf + hf)
-
-    results = {'Power VLF (ms2)': vlf,
-               'Power LF (ms2)': lf,
-               'Power HF (ms2)': hf,
-               'Power Total (ms2)': total_power,
-               'LF/HF': (lf / hf),
-               'Peak VLF (Hz)': peak_vlf,
-               'Peak LF (Hz)': peak_lf,
-               'Peak HF (Hz)': peak_hf,
-               'Fraction LF (nu)': lf_nu,
-               'Fraction HF (nu)': hf_nu
-               }
-
-    return results, fxx, pxx
+def frequency_domain(dataframe, start_index: int, end_index: int) -> dict:
+    low_frequency_signal = dataframe["[B] HRV-LF Power (0.04-0.16 Hz)"].iloc[start_index:end_index]
+    high_frequency_signal = dataframe["[B] HRV-HF Power (0.16-0.4 Hz)"].iloc[start_index:end_index]
+    lf_power = low_frequency_signal.sum() / ECG_SAMPLE_RATE
+    hf_power = high_frequency_signal.sum() / ECG_SAMPLE_RATE
+    total_power = lf_power + hf_power
+    features = {
+        "Power LF (ms2)": lf_power,
+        "Power HF (ms2)": hf_power,
+        "Power Total (ms2)": total_power,
+        "LF/HF": (lf_power / hf_power),
+        "Peak LF (Hz)": np.max(low_frequency_signal),
+        "Peak HF (Hz)": np.max(high_frequency_signal),
+    }
+    return features
 
 
 df = create_clean_dataframe(101)
-print(mean_hr(df, 0, -1))
-r_peaks = detect_r_peaks(df, plot=False)
-rr_intervals = compute_rr_intervals(r_peaks, plot=False)
-rr_interpolated = interpolate_rr_intervals(rr_intervals)
-# features_frequency_domain, fxx, pxx = frequency_domain(rr_interpolated)
-# features_time_domain = time_domain_features(rr_intervals)
+# features_frequency_domain = frequency_domain(df, 0, -1)
+# features_time_domain = time_domain_features(df, 0, -1)
 
 print("Time domain metrics - automatically corrected RR-intervals:")
-for k, v in time_domain_features(rr_intervals).items():
+for k, v in time_domain_features(df, 23000, 43000).items():
     print("- %s: %.2f" % (k, v))
+print()
 
-print("Frequency domain metrics:")
-results, fxx, pxx = frequency_domain(rr_interpolated)
-for k, v in results.items():
+print("Frequency domain metrics - automatically corrected RR-intervals:")
+for k, v in frequency_domain(df, 23000, 43000).items():
     print("- %s: %.2f" % (k, v))
+print()
 
-# condition_indexes = synchronize_all_conditions(102)
-# for key, value in condition_indexes.items():
-#     print(f"The mean heart rate for condition {key} is {mean_hr(clean_dataframe, value[0], value[1])}")
-# plt.plot(clean_dataframe["Sensor-B:EEG"])
-# plt.show()
